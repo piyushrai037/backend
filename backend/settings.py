45ceaf29073533673c5253443678bc4d8a164e09
piyushrai037 <126897086+piyushrai037@users.noreply.github.com>
2024-09-02 23:56:13 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -120,6 +120,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "https://edunova-three.vercel.app",
     "http://localhost:3000",
 ]
 # Google Cloud Storage settings
